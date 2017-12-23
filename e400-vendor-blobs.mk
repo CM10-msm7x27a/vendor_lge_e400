7083aae07ef349798bd691241e22c8c94db2f9f0
ZeeRooo <chavesjuan400@gmail.com>
2017-12-23 16:03:48 -0300

    Yeah, the L3 is alive!

--- e400-vendor-blobs.mk
+++ e400-vendor-blobs.mk
@@ -15,103 +15,91 @@
 # This file was generated with generate_makefile.py
 
 PRODUCT_COPY_FILES += \
-	vendor/lge/e400/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
 	vendor/lge/e400/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+	vendor/lge/e400/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
 	vendor/lge/e400/proprietary/etc/firmware/wlan/volans/WCN1314_qcom_fw.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_fw.bin \
-	vendor/lge/e400/proprietary/etc/firmware/wlan/volans/WCN1314_cfg.dat:system/etc/firmware/wlan/volans/WCN1314_cfg.dat \
 	vendor/lge/e400/proprietary/etc/firmware/wlan/volans/WCN1314_qcom_wlan_nv.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_wlan_nv.bin \
-	vendor/lge/e400/proprietary/bin/qmiproxy:system/bin/qmiproxy \
-	vendor/lge/e400/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
-	vendor/lge/e400/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-	vendor/lge/e400/proprietary/bin/netmgrd:system/bin/netmgrd \
-	vendor/lge/e400/proprietary/bin/rild:system/bin/rild \
-	vendor/lge/e400/proprietary/bin/qmuxd:system/bin/qmuxd \
-	vendor/lge/e400/proprietary/bin/sensord:system/bin/sensord \
-	vendor/lge/e400/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-	vendor/lge/e400/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
-	vendor/lge/e400/proprietary/lib/libqmi.so:system/lib/libqmi.so \
-	vendor/lge/e400/proprietary/lib/libOmxAmrDec.so:system/lib/libOmxAmrDec.so \
-	vendor/lge/e400/proprietary/lib/libdsm.so:system/lib/libdsm.so \
-	vendor/lge/e400/proprietary/lib/liblgdrm.so:system/lib/liblgdrm.so \
-	vendor/lge/e400/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
-	vendor/lge/e400/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
-	vendor/lge/e400/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
-	vendor/lge/e400/proprietary/lib/libwms.so:system/lib/libwms.so \
-	vendor/lge/e400/proprietary/lib/libwfcu.so:system/lib/libwfcu.so \
-	vendor/lge/e400/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
-	vendor/lge/e400/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
-	vendor/lge/e400/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
-	vendor/lge/e400/proprietary/lib/libOmxEvrcHwDec.so:system/lib/libOmxEvrcHwDec.so \
-	vendor/lge/e400/proprietary/lib/libauth.so:system/lib/libauth.so \
-	vendor/lge/e400/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
-	vendor/lge/e400/proprietary/lib/libchromatix_hi542_preview.so:system/lib/libchromatix_hi542_preview.so \
-	vendor/lge/e400/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-	vendor/lge/e400/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
-	vendor/lge/e400/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
-	vendor/lge/e400/proprietary/lib/libOmxQcelpHwDec.so:system/lib/libOmxQcelpHwDec.so \
-	vendor/lge/e400/proprietary/lib/libaudioflinger.so:system/lib/libaudioflinger.so \
+	vendor/lge/e400/proprietary/etc/firmware/wlan/volans/WCN1314_cfg.dat:system/etc/firmware/wlan/volans/WCN1314_cfg.dat \
+	vendor/lge/e400/proprietary/etc/firmware/wlan/volans/WCN1314_qcom_cfg.ini:system/etc/firmware/wlan/volans/WCN1314_qcom_cfg.ini \
+	vendor/lge/e400/proprietary/lib/libcamera.so:system/lib/libcamera.so \
+	vendor/lge/e400/proprietary/lib/libOmxrv9Dec.so:system/lib/libOmxrv9Dec.so \
 	vendor/lge/e400/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
-	vendor/lge/e400/proprietary/lib/libaudioutils.so:system/lib/libaudioutils.so \
-	vendor/lge/e400/proprietary/lib/liblgeat.so:system/lib/liblgeat.so \
+	vendor/lge/e400/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+	vendor/lge/e400/proprietary/lib/libnv.so:system/lib/libnv.so \
+	vendor/lge/e400/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
+	vendor/lge/e400/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+	vendor/lge/e400/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
 	vendor/lge/e400/proprietary/lib/liboem_rapi.so:system/lib/liboem_rapi.so \
-	vendor/lge/e400/proprietary/lib/libtcpfinaggr.so:system/lib/libtcpfinaggr.so \
-	vendor/lge/e400/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+	vendor/lge/e400/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
+	vendor/lge/e400/proprietary/lib/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
+	vendor/lge/e400/proprietary/lib/libwcnftm.so:system/lib/libwcnftm.so \
+	vendor/lge/e400/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
+	vendor/lge/e400/proprietary/lib/libqueue.so:system/lib/libqueue.so \
+	vendor/lge/e400/proprietary/lib/libOmxQcelpHwDec.so:system/lib/libOmxQcelpHwDec.so \
+	vendor/lge/e400/proprietary/lib/libwfcu.so:system/lib/libwfcu.so \
+	vendor/lge/e400/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
+	vendor/lge/e400/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
+	vendor/lge/e400/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+	vendor/lge/e400/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
+	vendor/lge/e400/proprietary/lib/libOmxIttiamVdec.so:system/lib/libOmxIttiamVdec.so \
+	vendor/lge/e400/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
+	vendor/lge/e400/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
+	vendor/lge/e400/proprietary/lib/libOmxEvrcHwDec.so:system/lib/libOmxEvrcHwDec.so \
+	vendor/lge/e400/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+	vendor/lge/e400/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
+	vendor/lge/e400/proprietary/lib/liblge_security.so:system/lib/liblge_security.so \
+	vendor/lge/e400/proprietary/lib/liblgeat.so:system/lib/liblgeat.so \
+	vendor/lge/e400/proprietary/lib/libwms.so:system/lib/libwms.so \
+	vendor/lge/e400/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+	vendor/lge/e400/proprietary/lib/libOmxQcelp13Dec.so:system/lib/libOmxQcelp13Dec.so \
+	vendor/lge/e400/proprietary/lib/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
+	vendor/lge/e400/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
+	vendor/lge/e400/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+	vendor/lge/e400/proprietary/lib/libqmi.so:system/lib/libqmi.so \
+	vendor/lge/e400/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
+	vendor/lge/e400/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+	vendor/lge/e400/proprietary/lib/libril.so:system/lib/libril.so \
+	vendor/lge/e400/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
+	vendor/lge/e400/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+	vendor/lge/e400/proprietary/lib/libcm.so:system/lib/libcm.so \
+	vendor/lge/e400/proprietary/lib/libOmxAmrDec.so:system/lib/libOmxAmrDec.so \
+	vendor/lge/e400/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
+	vendor/lge/e400/proprietary/lib/libauth.so:system/lib/libauth.so \
+	vendor/lge/e400/proprietary/lib/libOmxVp8Dec.so:system/lib/libOmxVp8Dec.so \
+	vendor/lge/e400/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
+	vendor/lge/e400/proprietary/lib/libdsm.so:system/lib/libdsm.so \
 	vendor/lge/e400/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
 	vendor/lge/e400/proprietary/lib/libidl.so:system/lib/libidl.so \
-	vendor/lge/e400/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
-	vendor/lge/e400/proprietary/lib/libcm.so:system/lib/libcm.so \
-	vendor/lge/e400/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
-	vendor/lge/e400/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-	vendor/lge/e400/proprietary/lib/libOmxVp8Dec.so:system/lib/libOmxVp8Dec.so \
-	vendor/lge/e400/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
-	vendor/lge/e400/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
-	vendor/lge/e400/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-	vendor/lge/e400/proprietary/lib/liblge_security.so:system/lib/liblge_security.so \
-	vendor/lge/e400/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
-	vendor/lge/e400/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+	vendor/lge/e400/proprietary/lib/libchromatix_hi542_preview.so:system/lib/libchromatix_hi542_preview.so \
+	vendor/lge/e400/proprietary/lib/liblgdrm.so:system/lib/liblgdrm.so \
+	vendor/lge/e400/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
 	vendor/lge/e400/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-	vendor/lge/e400/proprietary/lib/libOmxAdpcmDec.so:system/lib/libOmxAdpcmDec.so \
-	vendor/lge/e400/proprietary/lib/tcp-connections.so:system/lib/tcp-connections.so \
-	vendor/lge/e400/proprietary/lib/pp_proc_plugin.so:system/lib/pp_proc_plugin.so \
-	vendor/lge/e400/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-	vendor/lge/e400/proprietary/lib/libOmxQcelp13Dec.so:system/lib/libOmxQcelp13Dec.so \
-	vendor/lge/e400/proprietary/lib/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
-	vendor/lge/e400/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
-	vendor/lge/e400/proprietary/lib/libwcnftm.so:system/lib/libwcnftm.so \
-	vendor/lge/e400/proprietary/lib/libril.so:system/lib/libril.so \
-	vendor/lge/e400/proprietary/lib/libdiag.so:system/lib/libdiag.so \
-	vendor/lge/e400/proprietary/lib/qnet-plugin.so:system/lib/qnet-plugin.so \
-	vendor/lge/e400/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
-	vendor/lge/e400/proprietary/lib/libqdp.so:system/lib/libqdp.so \
-	vendor/lge/e400/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-	vendor/lge/e400/proprietary/lib/libOmxCore.so:system/lib/libOmxCore.so \
-	vendor/lge/e400/proprietary/lib/libdnshostprio.so:system/lib/libdnshostprio.so \
-	vendor/lge/e400/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-	vendor/lge/e400/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
-	vendor/lge/e400/proprietary/lib/libnv.so:system/lib/libnv.so \
-	vendor/lge/e400/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
-	vendor/lge/e400/proprietary/lib/libOmxOn2Dec.so:system/lib/libOmxOn2Dec.so \
-	vendor/lge/e400/proprietary/lib/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
-	vendor/lge/e400/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
-	vendor/lge/e400/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
-	vendor/lge/e400/proprietary/lib/libOmxrv9Dec.so:system/lib/libOmxrv9Dec.so \
-	vendor/lge/e400/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
-	vendor/lge/e400/proprietary/lib/libqdi.so:system/lib/libqdi.so \
-	vendor/lge/e400/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
-	vendor/lge/e400/proprietary/lib/lib_LG_fastaf.so:system/lib/lib_LG_fastaf.so \
 	vendor/lge/e400/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
-	vendor/lge/e400/proprietary/lib/libOmxIttiamVdec.so:system/lib/libOmxIttiamVdec.so \
-	vendor/lge/e400/proprietary/lib/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so \
+	vendor/lge/e400/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+	vendor/lge/e400/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+	vendor/lge/e400/proprietary/lib/libOmxOn2Dec.so:system/lib/libOmxOn2Dec.so \
+	vendor/lge/e400/proprietary/lib/libOmxCore.so:system/lib/libOmxCore.so \
+	vendor/lge/e400/proprietary/lib/libOmxAdpcmDec.so:system/lib/libOmxAdpcmDec.so \
+	vendor/lge/e400/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
+	vendor/lge/e400/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
+	vendor/lge/e400/proprietary/lib/libqdp.so:system/lib/libqdp.so \
+	vendor/lge/e400/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
+	vendor/lge/e400/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
 	vendor/lge/e400/proprietary/lib/hw/sensors.e0.so:system/lib/hw/sensors.e0.so \
+	vendor/lge/e400/proprietary/lib/hw/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so \
 	vendor/lge/e400/proprietary/lib/hw/lights.msm7x27a.so:system/lib/hw/lights.msm7x27a.so \
-	vendor/lge/e400/proprietary/lib/hw/audio_policy.msm7x27a.so:system/lib/hw/audio_policy.msm7x27a.so \
-	vendor/lge/e400/proprietary/lib/hw/audio.primary.msm7x27a.so:system/lib/hw/audio.primary.msm7x27a.so \
-	vendor/lge/e400/proprietary/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
-	vendor/lge/e400/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
 	vendor/lge/e400/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-	vendor/lge/e400/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+	vendor/lge/e400/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+	vendor/lge/e400/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+	vendor/lge/e400/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+	vendor/lge/e400/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
 	vendor/lge/e400/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
 	vendor/lge/e400/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-	vendor/lge/e400/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-	vendor/lge/e400/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-	vendor/lge/e400/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so
+	vendor/lge/e400/proprietary/bin/qmuxd:system/bin/qmuxd \
+	vendor/lge/e400/proprietary/bin/qmiproxy:system/bin/qmiproxy \
+	vendor/lge/e400/proprietary/bin/rild:system/bin/rild \
+	vendor/lge/e400/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+	vendor/lge/e400/proprietary/bin/netmgrd:system/bin/netmgrd \
+	vendor/lge/e400/proprietary/bin/sensord:system/bin/sensord \
+	vendor/lge/e400/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+	vendor/lge/e400/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init
